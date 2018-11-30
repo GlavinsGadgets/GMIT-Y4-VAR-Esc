56c6d7f467da3d2d9768e29e7831e07ef9f68aea
Eanna Glavin <eannagglavin@gmail.com>
2018-11-30 00:26:58 +0000

    table

--- Esc/Assets/Level_01/Scripts/DoorScript.cs
+++ Esc/Assets/Level_01/Scripts/DoorScript.cs
@@ -4,8 +4,28 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour {
 
+    public bool opendoor = false;
 
+    private Animator _animator = null;
 
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+
+        
+    }
+    private void Update()
+    {
+        if (opendoor)
+        {
+            OnTriggerEnter();
+        }
+    }
+
+    private void OnTriggerEnter()
+    {
+        _animator.SetBool("isopen", true);
+    }
 
 
 }
