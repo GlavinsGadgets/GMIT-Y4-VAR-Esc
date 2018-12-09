1a66e81a416b55c6bff030c0e5bf0d87772c190b
Eanna Glavin <eannagglavin@gmail.com>
2018-12-09 04:00:45 +0000

    room 2 start

---
+++ Esc/Assets/Room_02/Scripts/Room_02_HallDoorControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room_02_HallDoorControl : MonoBehaviour {
+
+    /*
+        Door to open the door if the opendoor bool has been set to true.
+    */
+
+    // Inits vars
+    private Animator _animator = null;
+    public bool opendoor = false;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        // Checks to see if the opendoor is enabled then it will call the OpenDoor function
+        if (opendoor)
+        {
+            OpenDoor();
+        }
+        else if (!opendoor)
+        {
+            CloseDoor();
+        }
+        Move_Player Move = FindObjectOfType<Move_Player>();
+        if (Move.PlayerReachedPoint == true)
+        {
+            opendoor = false;
+            CloseDoor();
+        }
+    }
+
+    public void OpenDoor()
+    {
+        Door_Open_Sound OpenCall = FindObjectOfType<Door_Open_Sound>();
+        _animator.SetBool("isopen", true);
+        OpenCall.PlayOpenSound();
+    }
+    public void CloseDoor()
+    {
+        Door_Close_Sound CloseCall = FindObjectOfType<Door_Close_Sound>();
+        _animator.SetBool("isopen", false);
+        CloseCall.PlayCloseSound();
+    }
+}
---
+++ Esc/Assets/Room_02/Scripts/Room_02_Move_Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room_02_Move_Player : MonoBehaviour {
+
+    public bool startMovement = false;
+    public bool PlayerReachedInsideRoom = false;
+    private Vector3 InsideRoom = new Vector3(4f, 1.1f, 0f);
+    private float step;
+    public float stepspeed;
+
+    public bool Reset;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        step = stepspeed * Time.deltaTime;
+
+        if (startMovement)
+        { 
+            OnLoadMove();
+        }
+
+
+        if (gameObject.transform.position == InsideRoom)
+        {
+            PlayerReachedInsideRoom = true;
+        }
+        else if (gameObject.transform.position != InsideRoom)
+        {
+            PlayerReachedInsideRoom = false;
+        }
+
+
+        if (Reset)
+        {
+            OnLevelReset();
+        }
+    }
+
+    public void OnLoadMove()
+    {
+        gameObject.transform.position = Vector3.MoveTowards(transform.position, InsideRoom, step);
+    }
+    public void OnLevelReset()
+    {
+        gameObject.transform.position = Vector3.MoveTowards(transform.position, new Vector3(0, 1.1f, 0), (10 * Time.deltaTime));
+    }
+
+}
